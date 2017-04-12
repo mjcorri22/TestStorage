1dae01e4c30a4a2531f9d058a16733bc820e9fe2
mjcorri22 <mjcorri22@gmail.com>
2017-04-12 09:28:03 -0700

    Create main.cs
    Adding new cs file.

---
+++ main.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+
+namespace Test
+{
+  public class TestGitHub
+  {
+      private string s;
+      
+      public TestGitHub()
+      {
+        s = string.Empty;
+      }
+      
+      public TestGitHub(string input)
+      {
+        s = input;
+      }
+  }
+}
